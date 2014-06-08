56e8701a0d07f0f0083e8325d073c25f46580b8b
artsoroka <githubrepository@yandex.ru>
2014-06-08 23:41:15 +0400

    feature: get Procedures, urls are prepared before they passed to makeHTTPRequest

--- lib/etp.php
+++ lib/etp.php
@@ -5,19 +5,74 @@ class ETP {
 	protected $timeout; 
 	protected $xml; 
 	protected $result; 
+	protected $connection = false; 
+	protected $connection_options; 
+	protected $url; 
+	protected $http_response = array(); 
+
 	public function __construct($timeout = 10){
 		$this->timeout = $timeout; 
 	}
 
+	public function getProcedures(){
+		$this->url = "https://etp.gpb.ru/api/procedures.php"; 
+		$this->setUrlOffset(1); 	
+		$xml = $this->makeHTTPRequest(); 
+		
+		$this->parseData($xml); 
+
+
+	}
+
 	public function getCompanyList(){
+		$this->url = "https://etp.gpb.ru/api/company_list.php";
 		$xml = $this->makeHTTPRequest(); 
 		$this->parseData($xml); 
 		
 		return $this->result; 
 	}
 
-	private function makeHTTPRequest(){
+	private function setUrlOffset($offset = 0){
+
+		if($offset){
+			$this->url .= "?late=" . $offset; 
+		}
+
+	}
+
+	private function configureConnection($url = false){
 		
+		if ( ! $url || empty($url) ) throw new Exception("empty string instead of url", 1);
+
+		$this->connection_options = array(
+        	CURLOPT_RETURNTRANSFER => true,     // return web page
+        	CURLOPT_HEADER         => false,    // don't return headers
+        	CURLOPT_FOLLOWLOCATION => true,     // follow redirects
+        	CURLOPT_ENCODING       => "",       // handle all encodings
+        	CURLOPT_USERAGENT      => "badlab", // who am i
+        	CURLOPT_AUTOREFERER    => true,     // set referer on redirect
+        	CURLOPT_CONNECTTIMEOUT => 120,      // timeout on connect
+        	CURLOPT_TIMEOUT        => 120,      // timeout on response
+        	CURLOPT_MAXREDIRS      => 100,       // stop after 10 redirects
+        	CURLOPT_SSL_VERIFYPEER => false     // Disabled SSL Cert checks
+    	);
+
+		$this->connection = ($this->connection)?: curl_init($url); 
+		curl_setopt_array( $this->connection, $this->connection_options);
+
+	}
+
+	private function makeHTTPRequest(){
+		$this->configureConnection($this->url); 
+
+		$content = curl_exec( $this->connection );
+    	$err     = curl_errno( $this->connection );
+    	$errmsg  = curl_error( $this->connection );
+    	$header  = curl_getinfo( $this->connection );
+    	curl_close( $this->connection );		
+		
+    	return $content; 
+
 		return 
 		'<companies >
 			<company>
@@ -39,9 +94,11 @@ class ETP {
 		$this->validate($xml_string); 
 
 		$xml = simplexml_load_string($xml_string);
+/*
 		$this->result = $xml; 
 		return true; 
-		$json = json_encode($xml);
+
+*/		$json = json_encode($xml);
 		$this->result = json_decode($json,TRUE);
 	}
 
